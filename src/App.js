b0e48c81be58b20c0aad7cfef0c739ce9c25713d
Chinmay Anand <chinmayanand896@gmail.com>
2022-03-25 15:51:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen bg-gray-100 p-8'>
+        <div className=''>
+          <input className='p-4 text-gray-800 w-full mb-4' type="text" placeholder='Search...'/>
+         <div className='flex justify-center'>
+         <ul className='grid grid-cols-2 sm:grid-cols-5 gap-4 sm:gap-12'>
+            <li className='bg-white p-4 w-32 text-center rounded shadow-lg'>
+              John
+            </li>
+            <li className='bg-white p-4 w-32 text-center rounded shadow-lg'>
+              John2
+            </li>
+            <li className='bg-white p-4 w-32 text-center rounded shadow-lg'>
+              John3
+            </li>
+            <li className='bg-white p-4 w-32 text-center rounded shadow-lg'>
+              John4
+            </li>
+            <li className='bg-white p-4 w-32 text-center rounded shadow-lg'>
+              John5
+            </li>
+            <li className='bg-white p-4 w-32 text-center rounded shadow-lg'>
+              John6
+            </li>
+            <li className='bg-white p-4 w-32 text-center rounded shadow-lg'>
+              John7
+            </li>
+            <li className='bg-white p-4 w-32 text-center rounded shadow-lg'>
+              John8
+            </li>
+            <li className='bg-white p-4 w-32 text-center rounded shadow-lg'>
+              John9
+            </li>
+            <li className='bg-white p-4 w-32 text-center rounded shadow-lg'>
+              John10
+            </li>
+          </ul>
+         </div>
+        </div>
     </div>
   );
 }
